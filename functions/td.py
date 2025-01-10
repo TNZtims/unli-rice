58108db710939ae42d532f7f23c737b4cbcff970
TNZtims <timotniellau@gmail.com>
2025-01-11 04:16:57 +0800

    add 125 scale logic

--- functions/td.py
+++ functions/td.py
@@ -12,27 +12,33 @@ def resource_path(relative_path):
   else:
     return os.path.join(os.path.abspath("."), relative_path)
 
-image_path = resource_path('images/not_working_final.png')
-image_path_2 = resource_path('images/not_working_final_2.png')
+not_working_image_path_100 = resource_path('images/not_working_final.png')
+not_working_image_path_150 = resource_path('images/not_working_final_2.png')
+not_working_image_path_125 = resource_path('images/not_working_final_3.png')
 
 def td():
   show_toast("Auto Yes Bypass enabled!", 2000, "top-right", "warning")
   
   while True:
     try:
-      location1, location2 = None, None
+      location1, location2, location3 = None, None, None
       
       try:
-        location1 = pyautogui.locateOnScreen(image_path, confidence=0.5)
+        location1 = pyautogui.locateOnScreen(not_working_image_path_100, confidence=0.5)
       except pyautogui.ImageNotFoundException:
         pass
 
       try:
-        location2 = pyautogui.locateOnScreen(image_path_2, confidence=0.5)
+        location2 = pyautogui.locateOnScreen(not_working_image_path_150, confidence=0.5)
+      except pyautogui.ImageNotFoundException:
+        pass
+      
+      try:
+        location3 = pyautogui.locateOnScreen(not_working_image_path_125, confidence=0.5)
       except pyautogui.ImageNotFoundException:
         pass
 
-      if location1 is not None or location2 is not None:
+      if location1 is not None or location2 is not None or location3 is not None:
         break
 
     except Exception as e:
--- functions/td_ob.py
+++ functions/td_ob.py
@@ -13,27 +13,32 @@ def resource_path(relative_path):
   else:
     return os.path.join(os.path.abspath("."), relative_path)
 
-image_path_1 = resource_path('images/not_working_final.png')
-image_path_2 = resource_path('images/not_working_final_2.png')
-image_path_3 = resource_path('images/start_working_again.png')
-image_path_4 = resource_path('images/start_working_again_2.png')
+start_working_image_path_100 = resource_path('images/start_working_again.png')
+start_working_image_path_150 = resource_path('images/start_working_again_2.png')
+start_working_image_path_125 = resource_path('images/start_working_again_3.png')
 
-def td_ob(mins, secs):
+def td_ob(hours, mins, secs):
+  print(f'Hours: {hours}, Minutes: {mins}, Seconds: {secs}')
   while True:
     try:
-      location1, location2 = None, None
+      location1A, location2A, location3A = None, None, None
       
       try:
-        location1 = pyautogui.locateOnScreen(image_path_3, confidence=0.8)
+        location1A = pyautogui.locateOnScreen(start_working_image_path_100, confidence=0.8)
       except pyautogui.ImageNotFoundException:
         pass
 
       try:
-        location2 = pyautogui.locateOnScreen(image_path_4, confidence=0.8)
+        location2A = pyautogui.locateOnScreen(start_working_image_path_150, confidence=0.8)
+      except pyautogui.ImageNotFoundException:
+        pass
+      
+      try:
+        location3A = pyautogui.locateOnScreen(start_working_image_path_125, confidence=0.8)
       except pyautogui.ImageNotFoundException:
         pass
 
-      if location1 is not None or location2 is not None:
+      if location1A is not None or location2A is not None or location3A is not None:
         break
 
     except Exception as e:
@@ -44,12 +49,14 @@ def td_ob(mins, secs):
 
     time.sleep(1)
 
-  interval = timedelta(minutes=max(0, int(mins)-3), seconds=int(secs))
+  interval = timedelta(hours=int(hours), minutes=int(mins), seconds=int(secs)) - timedelta(minutes=3)
   start_time = datetime.now()
-  show_toast(f"Starting countdown {int(mins)-3} mins, {secs} secs", 3000, "top-right", "warning")
+  show_toast(f"Starting countdown {hours} hours, {mins} minutes, {secs} seconds (3 minutes subtracted)", 3000, "top-right", "warning")
 
-  secs = 0
-  mins = 0
+  elapsed_secs = 0
+  elapsed_mins = 0
+  elapsed_hours = 0
+
   while True:
     current_time = datetime.now()
     
@@ -59,30 +66,38 @@ def td_ob(mins, secs):
     if pyautogui.position() == (0, 0):
       break
     time.sleep(1)
-    secs+=1
+    elapsed_secs += 1
     
-    if secs == 60:
-      secs = 0
-      mins+=1
-      show_toast(f"{mins+3} minutes...", 2000, "top-right", "info")
-  
+    if elapsed_secs == 60:
+      elapsed_secs = 0
+      elapsed_mins += 1
+      if elapsed_mins % 60 == 0:
+        elapsed_mins = 0
+        elapsed_hours += 1
+      show_toast(f"{elapsed_hours} hours, {elapsed_mins + 3} minutes elapsed...", 2000, "top-right", "info")
+
   while True:
     try:
-      location3, location4 = None, None
+      location1B, location2B, location3B = None, None, None
       
       try:
-        location3 = pyautogui.locateCenterOnScreen(image_path_3, confidence=0.8)
+        location1B = pyautogui.locateCenterOnScreen(start_working_image_path_100, confidence=0.8)
       except pyautogui.ImageNotFoundException:
         pass
 
       try:
-        location4 = pyautogui.locateCenterOnScreen(image_path_4, confidence=0.8)
+        location2B = pyautogui.locateCenterOnScreen(start_working_image_path_150, confidence=0.8)
+      except pyautogui.ImageNotFoundException:
+        pass
+      
+      try:
+        location3B = pyautogui.locateCenterOnScreen(start_working_image_path_125, confidence=0.8)
       except pyautogui.ImageNotFoundException:
         pass
 
-      if location3 is not None or location4 is not None:
+      if location1B is not None or location2B is not None or location3B is not None:
         show_toast("Start working again...", 3000, "top-right", "info")
-        pyautogui.click(location3 or location4)
+        pyautogui.click(location1B or location2B or location3B)
         time.sleep(1)
         pyautogui.hotkey('win', 'down')
         time.sleep(1)
